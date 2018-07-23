3a3e37c3738f0177d78ccdf06107aa579b0582ab
Autumn <autumncbullard@gmail.com>
2018-07-23 13:38:57 -0400

    Add markup and styling for calculator screen

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,12 @@ export default class App extends Component {
         <header>
           <h1>Calculator</h1>
         </header>
+        <main>
+          <div className="calculator">
+            {/* CALCULATOR SCREEN */}
+            <div className="screen">0</div>
+          </div>
+        </main>
         {/* FOOTER */}
         <footer>Coded by <a href="../portfolio" target="_blank">Autumn Bullard</a></footer>
       </div>
