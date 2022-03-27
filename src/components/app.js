f34abaef5ed3ffbe2c5c6a762b03c3ec0e1f4b89
autumnchris <autumncbullard@gmail.com>
2022-03-27 12:44:40 -0400

    Switch to React Hooks and remove keyboard functionality

--- src/components/app.js
+++ src/components/app.js
@@ -1,315 +1,250 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CalcButton from './calc-button';
 
-class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      screenValue: '0',
-      calcButtons: [
-        {
-          value: '+/-',
-          func: () => this.togglePosNeg()
-        },
-        {
-          value: '%',
-          func: () => this.convertToPercent()
-        },
-        {
-          value: 'CE',
-          func: () => this.clearEntry(),
-          className: 'clear'
-        },
-        {
-          value: 'AC',
-          func: () => this.clearAll(),
-          className: 'clear'
-        },
-        {
-          value: '7',
-          func: () => this.selectNumber('7'),
-          className: 'number'
-        },
-        {
-          value: '8',
-          func: () => this.selectNumber('8'),
-          className: 'number'
-        },
-        {
-          value: '9',
-          func: () => this.selectNumber('9'),
-          className: 'number'
-        },
-        {
-          value: '&divide;',
-          func: () => this.selectOperator('/'),
-          className: 'operator'
-        },
-        {
-          value: '4',
-          func: () => this.selectNumber('4'),
-          className: 'number'
-        },
-        {
-          value: '5',
-          func: () => this.selectNumber('5'),
-          className: 'number'
-        },
-        {
-          value: '6',
-          func: () => this.selectNumber('6'),
-          className: 'number'
-        },
-        {
-          value: '&times;',
-          func: () => this.selectOperator('*'),
-          className: 'operator'
-        },
-        {
-          value: '1',
-          func: () => this.selectNumber('1'),
-          className: 'number'
-        },
-        {
-          value: '2',
-          func: () => this.selectNumber('2'),
-          className: 'number'
-        },
-        {
-          value: '3',
-          func: () => this.selectNumber('3'),
-          className: 'number'
-        },
-        {
-          value: '-',
-          func: () => this.selectOperator('-'),
-          className: 'operator'
-        },
-        {
-          value: '0',
-          func: () => this.selectNumber('0'),
-          className: 'number'
-        },
-        {
-          value: '.',
-          func: () => this.selectDecimal()
-        },
-        {
-          value: '=',
-          func: () => this.solveEquation()
-        },
-        {
-          value: '+',
-          func: () => this.selectOperator('+'),
-          className: 'operator'
-        }
-      ]
-    };
-    this.result = '0';
-    this.endsWithOperator = this.result.charAt(this.result.length - 1).match(/\s/);
-  }
-
-  selectNumber(number) {
-    this.result = this.result.split(' ');
-
-    if (this.result[this.result.length - 1] === '0') {
-      this.result[this.result.length - 1] = this.result[this.result.length - 1].substr(1);
+const App = () => {
+  const calcButtons = [
+    {
+      value: '+/-',
+      func: () => togglePosNeg()
+    },
+    {
+      value: '%',
+      func: () => convertToPercent()
+    },
+    {
+      value: 'CE',
+      func: () => clearEntry(),
+      className: 'clear'
+    },
+    {
+      value: 'AC',
+      func: () => clearAll(),
+      className: 'clear'
+    },
+    {
+      value: '7',
+      func: () => selectNumber('7'),
+      className: 'number'
+    },
+    {
+      value: '8',
+      func: () => selectNumber('8'),
+      className: 'number'
+    },
+    {
+      value: '9',
+      func: () => selectNumber('9'),
+      className: 'number'
+    },
+    {
+      value: '&divide;',
+      func: () => selectOperator('/'),
+      className: 'operator'
+    },
+    {
+      value: '4',
+      func: () => selectNumber('4'),
+      className: 'number'
+    },
+    {
+      value: '5',
+      func: () => selectNumber('5'),
+      className: 'number'
+    },
+    {
+      value: '6',
+      func: () => selectNumber('6'),
+      className: 'number'
+    },
+    {
+      value: '&times;',
+      func: () => selectOperator('*'),
+      className: 'operator'
+    },
+    {
+      value: '1',
+      func: () => selectNumber('1'),
+      className: 'number'
+    },
+    {
+      value: '2',
+      func: () => selectNumber('2'),
+      className: 'number'
+    },
+    {
+      value: '3',
+      func: () => selectNumber('3'),
+      className: 'number'
+    },
+    {
+      value: '-',
+      func: () => selectOperator('-'),
+      className: 'operator'
+    },
+    {
+      value: '0',
+      func: () => selectNumber('0'),
+      className: 'number'
+    },
+    {
+      value: '.',
+      func: () => selectDecimal()
+    },
+    {
+      value: '=',
+      func: () => solveEquation()
+    },
+    {
+      value: '+',
+      func: () => selectOperator('+'),
+      className: 'operator'
     }
-    this.result[this.result.length - 1] += number;
-    this.setState({
-      screenValue: this.result[this.result.length - 1]
-    });
-    this.result = this.result.join(' ');
-  }
+  ];
 
-  selectOperator(operator) {
-    this.solveEquation();
+  const [screenValue, setScreenValue] = useState('0');
+  const [numA, setNumA] = useState('');
+  const [numB, setNumB] = useState('');
+  const [operator, setOperator] = useState('');
 
-    if (this.endsWithOperator) {
-      this.result = this.result.substr(0, this.result.length - 3);
-    }
-    this.result += ` ${operator} `;
-  }
+  function selectNumber(number) {
+    let currentNumA = numA;
+    let currentNumB = numB;
 
-  togglePosNeg() {
-
-    if (!this.endsWithOperator) {
-      this.result = this.result.split(' ');
-
-      if (this.result[this.result.length - 1] < 0) {
-        this.result[this.result.length - 1] = Math.abs(this.result[this.result.length - 1]);
-      }
-      else if (this.result[this.result.length - 1] > 0) {
-        this.result[this.result.length - 1] = -this.result[this.result.length - 1];
-      }
-      else {
-        this.result[this.result.length - 1] = 0;
-      }
-      this.setState({
-        screenValue: this.result[this.result.length - 1]
-      });
-      this.result = this.result.join(' ');
-    }
-  }
-
-  convertToPercent() {
-
-    if (!this.endsWithOperator) {
-      this.result = this.result.split(' ');
-      this.result[this.result.length - 1] /= 100;
-      this.setState({
-        screenValue: this.result[this.result.length - 1]
-      });
-      this.result = this.result.join(' ');
-    }
-  }
-
-  clearEntry() {
-
-    if (!this.endsWithOperator) {
-      this.result = this.result.split(' ');
-      this.result[this.result.length - 1] = '0';
-      this.setState({
-        screenValue: this.result[this.result.length - 1]
-      });
-      this.result = this.result.join(' ');
+    if (!operator) {
+      if(currentNumA === '0') currentNumA = ''; 
+      currentNumA += number;
+      setNumA(currentNumA);
+      setScreenValue(currentNumA);
     }
     else {
-      this.result = this.result.substr(0, this.result.length - 3);
+      if(currentNumB === '0') currentNumB = ''; 
+      currentNumB += number;
+      setNumB(currentNumB);
+      setScreenValue(currentNumB);
     }
   }
 
-  clearAll() {
-    this.result = '0';
-    this.setState({
-      screenValue: '0'
-    });
+  function selectOperator(operatorValue) {
+    if(numB) solveEquation();
+    if(numA) setOperator(operatorValue);
   }
 
-  selectDecimal() {
+  function solveEquation() {
+    const a = Number(numA);
+    const b = Number(numB);
+    let formula;
 
-    if (!this.endsWithOperator) {
-      this.result = this.result.split(' ');
+    if (numB && operator) {
 
-      if (!this.result[this.result.length - 1].includes('.')){
-        this.result[this.result.length - 1] += '.';
-        this.setState({
-          screenValue: this.result[this.result.length - 1]
-        });
-      }
-      this.result = this.result.join(' ');
-    }
-  }
-
-  solveEquation() {
-    let a;
-    let b;
-
-    if (this.result.includes(' ') && !this.endsWithOperator) {
-      this.result = this.result.split(' ');
-      a = Number(this.result[0]);
-      b = Number(this.result[2]);
-
-      switch (this.result[1]) {
+      switch (operator) {
         case '+':
-          this.result = a + b;
+          formula = a + b;
           break;
         case '-':
-          this.result = a - b;
+          formula = a - b;
           break;
         case '*':
-          this.result = a * b;
+          formula = a * b;
           break;
         case '/':
-          this.result = a / b;
+          formula = a / b;
       }
-      this.result = this.result.toString();
-      this.setState({
-        screenValue: this.result
-      });
+      formula = formula.toString();
+      setNumA(formula);
+      setNumB('');
+      setOperator('');
+      setScreenValue(formula);
     }
   }
 
-  setKeys(event) {
-    event.preventDefault();
+  function selectDecimal() {
+    let currentNumA = numA;
+    let currentNumB = numB;
 
-    if (event.shiftKey) {
-      switch (event.keyCode) {
-        // plus sign
-        case 187:
-          this.selectOperator(event.key);
-          break;
-        // multiplication sign
-        case 56:
-          this.selectOperator(event.key);
-          break;
-        // percentage
-        case 53:
-          this.convertToPercent();
-      }
+    if (currentNumB && !currentNumB.includes('.')) {
+      screenValue === '0' ? currentNumB = '0.' : currentNumB += '.';
+      setNumB(currentNumB);
+      setScreenValue(currentNumB);
+    }
+    else if (!operator && !currentNumB && !currentNumA.includes('.')) {
+      screenValue === '0' ? currentNumA = '0.' : currentNumA += '.';
+      setNumA(currentNumA);
+      setScreenValue(currentNumA);
+    }
+  }
+
+  function convertToPercent() {
+    let currentNumA = numA;
+    let currentNumB = numB;
+
+    if (numB) {
+      currentNumB /= 100;
+      currentNumB = currentNumB.toString();
+      setNumB(currentNumB);
+      setScreenValue(currentNumB);
+    }
+    else if (!operator && !numB) {
+      currentNumA /= 100;
+      currentNumA = currentNumA.toString();
+      setNumA(currentNumA);
+      setScreenValue(currentNumA);
+    }
+  }
+
+  function togglePosNeg() {
+    let currentNumA = numA;
+    let currentNumB = numB;
+    
+    if (currentNumB) {
+      currentNumB < 0 ? currentNumB = Math.abs(currentNumB) : currentNumB > 0 ? currentNumB = -currentNumB : currentNumB = 0;
+      currentNumB = currentNumB.toString();
+      setNumB(currentNumB);
+      setScreenValue(currentNumB);
+    }
+    else if (!operator && !currentNumB) {
+      currentNumA < 0 ? currentNumA = Math.abs(currentNumA) : currentNumA > 0 ? currentNumA = -currentNumA : currentNumA = 0;
+      currentNumA = currentNumA.toString();
+      setNumA(currentNumA);
+      setScreenValue(currentNumA);
+    }
+  }
+
+  function clearEntry() {
+
+    if (operator && numB) {
+      setNumB('');
+      setScreenValue('0');
+    }
+    else if (!operator) {
+      setNumA('');
+      setScreenValue('0');
     }
     else {
-      if (event.keyCode >= 48 && event.keyCode <= 57) {
-        this.selectNumber(event.key);
-      }
-      else {
-        switch (event.keyCode) {
-          // minus sign
-          case 189:
-            this.selectOperator(event.key);
-            break;
-          // division sign
-          case 191:
-            this.selectOperator(event.key);
-            break;
-          // equal sign
-          case 187:
-            this.solveEquation();
-            break;
-          // option/alt
-          case 18:
-            this.togglePosNeg();
-            break;
-          // delete/backspace
-          case 8:
-            this.clearEntry();
-            break;
-          // escape
-          case 27:
-            this.clearAll();
-            break;
-          // decimal/period
-          case 190:
-            this.selectDecimal();
-            break;
-        }
-      }
+      setOperator('');
     }
   }
 
-  componentDidMount() {
-    window.addEventListener('keydown', (event) => {
-      this.setKeys(event);
-    });
+  function clearAll() {
+    setNumA('');
+    setNumB('');
+    setOperator('');
+    setScreenValue('0');
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        <header>
-          <h1>Calculator</h1>
-        </header>
-        <main>
-          <div className="calculator">
-            <div className="screen">{this.state.screenValue}</div>
-            <div className="calc-buttons-container">{this.state.calcButtons.map((calcButton, index) => <CalcButton key={index} calcButton={calcButton} />)}</div>
-          </div>
-        </main>
-        <footer>Created by <a href="https://autumnchris.github.io/portfolio" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <header>
+      <h1>Calculator</h1>
+      </header>
+      <main>
+        <div className="calculator">
+          <div className="screen" id="display">{screenValue}</div>
+          <div className="calc-buttons-container">{calcButtons.map((calcButton, index) => <CalcButton key={index} calcButton={calcButton} />)}</div>
+        </div>
+      </main>
+      <footer>Created by <a href="https://autumnchris.github.io/portfolio" target="_blank">Autumn Bullard</a> &copy; {new Date().getFullYear()}</footer>
+    </React.Fragment>
+  );
 }
 
 export default App;
